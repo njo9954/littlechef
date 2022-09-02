093a2a38281c3031bf2fb4a8f9e9d67c28cc792f
hanny199 <hanny00156@gmail.com>
2022-09-02 16:48:45 +0900

    no message

---
+++ src/main/java/com/project/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller  
+public class MainController {
+
+	//메인홈페이지 
+	@GetMapping({"","/"})
+	public String main() {
+		return "main";
+	}
+}
