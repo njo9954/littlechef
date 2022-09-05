b1c777191c2f018d9ec6d17265158adf6f807678
hanny199 <hanny00156@gmail.com>
2022-09-05 13:27:28 +0900

    MemberController 수정 및 littlechef_sql .txt 파일 추가

--- src/main/java/com/project/controller/MemberController.java
+++ src/main/java/com/project/controller/MemberController.java
@@ -3,8 +3,10 @@ package com.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.project.domain.Member;
 import com.project.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,15 @@ public class MemberController {
 	@GetMapping("join")
 	public String join() {
 		return "/memberView/join";
+	}
+	
+	@PostMapping("join")
+	public String join(Member member) {
+		log.debug("{}",member);
+		
+		//service.insertMember(member);
+		
+		return "redirect:/";
 	}
 	
 	@GetMapping("login")
