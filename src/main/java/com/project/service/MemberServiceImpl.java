8c4b8df0bc57f2613b0c9c00d068b8f9828ab482
hanny199 <hanny00156@gmail.com>
2022-09-07 08:37:34 +0900

    웹시큐리티 삭제 및 원상복구

--- src/main/java/com/project/service/MemberServiceImpl.java
+++ src/main/java/com/project/service/MemberServiceImpl.java
@@ -9,17 +9,10 @@ import com.project.domain.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService {
-	@Autowired
-	private MemberDAO memberDAO;
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+
 	@Override
 	public int insertMember(Member member) {
-		String encodedPassword = passwordEncoder.encode(member.getM_pw()); // 암호화해주는 메소드 // 사용자가 입력한 비밀번호 암호화해서 전달
-		member.setM_pw(encodedPassword);// 그 멤버에 도로 집어넣음. 암호화된 다른 비밀번호로 set
-		
-		int result = memberDAO.insertMember(member);
-		return result;
+		return 0;
 	}
 	
 }
