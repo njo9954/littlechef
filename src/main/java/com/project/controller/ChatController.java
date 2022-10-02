4585776919551b9e0afd6664643023a2933a9e91
hanny199 <hanny00156@gmail.com>
2022-10-03 03:04:30 +0900

    채팅프로그램 (미완 )

--- src/main/java/com/project/controller/ChatController.java
+++ src/main/java/com/project/controller/ChatController.java
@@ -1,16 +1,27 @@
 package com.project.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 @RequestMapping("chatView")
 public class ChatController {
 
-	@GetMapping("chat")
-	public String chat() {
-		return "chatView/chat";
+	@GetMapping("/chat")
+	public void chat(Model model, @AuthenticationPrincipal UserDetails user) {
+		
+		
+		log.info("==================================");
+		log.info("@ChatController, GET Chat / Username : " + user.getUsername());
+		
+		model.addAttribute("user", user.getUsername());
+	}
+}
 
-}
-}
