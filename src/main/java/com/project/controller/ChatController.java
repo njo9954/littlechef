78d5599ddd1e428952a5a224b7a7317edb368927
hanny199 <hanny00156@gmail.com>
2022-10-03 00:36:14 +0900

    chatting controller chat 구현 (준비)

--- src/main/java/com/project/controller/ChatController.java
+++ src/main/java/com/project/controller/ChatController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import lombok.extern.slf4j.Slf4j;
-//@Slf4j
-@RequestMapping("eventView")
 @Controller
-public class EventController {
-	@GetMapping("event")
-	public String event() {
-		
-		return "eventView/event";
-	
-	}
+@RequestMapping("chatView")
+public class ChatController {
+
+	@GetMapping("chat")
+	public String chat() {
+		return "chatView/chat";
+
+}
 }
