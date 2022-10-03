1a7100392f74957a2e7ba68c41bbc445fd17ecbe
hanny199 <hanny00156@gmail.com>
2022-10-04 08:55:08 +0900

    수정 cart 삭제

--- src/main/java/com/project/controller/OrderController.java
+++ src/main/java/com/project/controller/OrderController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.project.domain.Cart;
 import com.project.domain.Food;
 import com.project.domain.FoodDetail;
 import com.project.domain.Ingredient;
